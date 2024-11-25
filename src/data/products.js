3e56375ef4bb3a07fb03ecbbd25b950c85b1e0cf
Deyvi777 <deyvi77777@gmail.com>
2024-11-25 01:20:41 -0400

    primer commit

---
+++ src/data/products.js
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    id: "product-1",
+    title: "Batido de Manzana",
+    img: "../../images/batidoManzana.webp",
+    description:
+      "Lorem ipson dolor sit amet consectetur adipisicing elit. Voluptatem, quia? Quidem, voluptas. Aspernatur, doloremque. Dolorum, quos. Lorem ipson dolor sit amet consectetur adipisicing elit. Voluptatem, quia? Quidem, voluptas. Aspernatur, doloremque. Dolorum, quos.",
+    price: 18,
+  },
+  {
+    id: "product-2",
+    title: "Batido de Chocolate",
+    img: "../../images/batidoChocolate.webp",
+    description:
+      "Lorem ipson dolor sit amet consectetur adipisicing elit. Voluptatem, quia? Quidem, voluptas. Aspernatur, doloremque. Dolorum, quos. Lorem ipson dolor sit amet consectetur adipisicing elit. Voluptatem, quia? Quidem, voluptas. Aspernatur, doloremque. Dolorum, quos.",
+    price: 18,
+  },
+  {
+    id: "product-3",
+    title: "Batido de Frutilla",
+    img: "../../images/batidoFrutilla.webp",
+    description:
+      "Lorem ipson dolor sit amet consectetur adipisicing elit. Voluptatem, quia? Quidem, voluptas. Aspernatur, doloremque. Dolorum, quos. Lorem ipson dolor sit amet consectetur adipisicing elit. Voluptatem, quia? Quidem, voluptas. Aspernatur, doloremque. Dolorum, quos.",
+    price: 18,
+  },
+  {
+    id: "product-4",
+    title: "Wafle con helado",
+    img: "../../images/wafleHelado.webp",
+    description:
+      "Lorem ipson dolor sit amet consectetur adipisicing elit. Voluptatem, quia? Quidem, voluptas. Aspernatur, doloremque. Dolorum, quos. Lorem ipson dolor sit amet consectetur adipisicing elit. Voluptatem, quia? Quidem, voluptas. Aspernatur, doloremque. Dolorum, quos.",
+    price: 15,
+  },
+  {
+    id: "product-5",
+    title: "Cuñapes",
+    img: "../../images/cunape.webp",
+    description:
+      "Lorem ipson dolor sit amet consectetur adipisicing elit. Voluptatem, quia? Quidem, voluptas. Aspernatur, doloremque. Dolorum, quos. Lorem ipson dolor sit amet consectetur adipisicing elit. Voluptatem, quia? Quidem, voluptas. Aspernatur, doloremque. Dolorum, quos.",
+    price: 10,
+  },
+  {
+    id: "product-6",
+    title: "Té",
+    img: "../../images/te.webp",
+    description:
+      "Lorem ipson dolor sit amet consectetur adipisicing elit. Voluptatem, quia? Quidem, voluptas. Aspernatur, doloremque. Dolorum, quos. Lorem ipson dolor sit amet consectetur adipisicing elit. Voluptatem, quia? Quidem, voluptas. Aspernatur, doloremque. Dolorum, quos.",
+    price: 11,
+  },
+];
